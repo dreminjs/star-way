72f828a6b0de6b3ea503da35ae062ce4f4ccc4fe
Andrey Dremin <sankoa230@gmail.com>
2024-12-02 00:58:10 +0300

    one more fix

--- src/features/star/ui/star-button.tsx
+++ src/features/star/ui/star-button.tsx
@@ -1,35 +1,39 @@
+import { RefObject, FC } from "react";
 
-import { RefObject, FC } from 'react';
-
-import Star from "../../../../src/assets/star.png"
+import Star from "../../../../src/assets/star.png";
 
 interface StarButtonProps {
-    spinning: boolean;
-    imgRef: RefObject<HTMLImageElement>;
-    handleMouseDown: () => void;
-    handleMouseUp: () => void;
+  spinning: boolean;
+  imgRef: RefObject<HTMLImageElement>;
+  handleMouseDown: () => void;
+  handleMouseUp: () => void;
 }
 
-export const StarButton: FC<StarButtonProps> = ({handleMouseDown, handleMouseUp, spinning, imgRef}) => {
-
-    return (
-        <button
-        className="mx-auto block"
-        onMouseDown={handleMouseDown}
-        onTouchStart={handleMouseDown}
-        onTouchEnd={handleMouseUp}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp} // Останавливаем вращение, если курсор уходит с кнопки
+export const StarButton: FC<StarButtonProps> = ({
+  handleMouseDown,
+  handleMouseUp,
+  spinning,
+  imgRef,
+}) => {
+  return (
+    <button
+      className="mx-auto block"
+      onMouseDown={handleMouseDown}
+      onTouchStart={handleMouseDown}
+      onTouchEnd={handleMouseUp}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      onContextMenu={(e) => e.preventDefault()} // Отключаем контекстное меню
+      onTouchMove={(e) => e.preventDefault()} // Останавливаем вращение, если курсор уходит с кнопки
     >
-        <img
-            ref={imgRef}
-            className={`w-[300px] spin ${spinning ? 'active' : ''}`}
-            src={Star}
-            alt="Star"
-            draggable="false"
-            onContextMenu={(e) => e.preventDefault()} 
-        
-        />
+      <img
+        ref={imgRef}
+        className={`w-[300px] spin ${spinning ? "active" : ""}`}
+        src={Star}
+        alt="Star"
+        draggable="false"
+        onContextMenu={(e) => e.preventDefault()}
+      />
     </button>
-    );
+  );
 };
