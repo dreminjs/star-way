5bcd3e5f148b2ffe9bc6630582871668b850d722
Andrey Dremin <sankoa230@gmail.com>
2024-12-03 22:04:05 +0300

    one more change

--- src/shared/api/api.instance.ts
+++ src/shared/api/api.instance.ts
@@ -1,9 +1,6 @@
 import { BASE_URL } from "../model/constants";
 import axios from "axios";
 
-
-
 export const instance = axios.create({
   baseURL: BASE_URL,
- 
 });
--- src/shared/api/services/prise.service.ts
+++ src/shared/api/services/prise.service.ts
@@ -3,12 +3,14 @@ import { instance } from "../api.instance";
 
 const data = Telegram.WebApp.initDataUnsafe;
 
+console.log(data)
+
 export const priseService = {
   axios: instance,
 
   async postResult(time: number) :Promise<IPriseResult> {
   
 
-    return (await this.axios.post("/general/check", {time}, {headers: { user_id: data.user?.id }})).data
+    return (await this.axios.post("/general/check", {time}, {headers: { "user_id": data.user?.id }})).data
   },
 };
