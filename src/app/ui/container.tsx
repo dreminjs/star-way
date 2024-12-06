fea6c83999b0207a073e41e6d22c806c2d2b6ea0
Andrey Dremin <94892594+dreminjs@users.noreply.github.com>
2024-12-06 11:45:58 +0300

    chore: container height

--- src/app/ui/container.tsx
+++ src/app/ui/container.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export const Container: FC<IProps> = ({ children }) => {
   const queryClient = new QueryClient();
   return (
-    <div className="max-w-[420px] mx-auto">
+    <div className="max-w-[420px] h-screen mx-auto">
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </div>
   );
