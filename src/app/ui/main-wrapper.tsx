db9f8cf39691ffd868d44052c710370b31a35d78
Andrey Dremin <sankoa230@gmail.com>
2024-12-07 23:47:19 +0300

    feat: loading pages

--- src/app/ui/main-wrapper.tsx
+++ src/app/ui/main-wrapper.tsx
@@ -1,13 +1,35 @@
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Container } from "./container";
+import FirstLoadingImg from "../../assets/loading-page-1.png";
+import SecondLoadingImg from "../../assets/loading-page-2.png";
 
 export const MainWrapper = () => {
-  console.log(window.Telegram.WebApp);
+  const [showSplash, setShowSplash] = useState(true);
+  const [currentImage, setCurrentImage] = useState(FirstLoadingImg);
+
+  useEffect(() => {
+    const imageTimer = setTimeout(() => {
+      setCurrentImage(SecondLoadingImg);
+    }, 3000); // Переключить изображение через 1.5 секунды
+
+    const splashTimer = setTimeout(() => {
+      setShowSplash(false);
+    }, 7000); // Скрыть картинку через 3 секунды
+
+    return () => {
+      clearTimeout(imageTimer);
+      clearTimeout(splashTimer);
+    };
+  }, []);
 
   return (
-    <div
-      className={`w-full h-svh mx-auto bg-[url('/public/main-bg.png')] bg-no-repeat bg-center bg-cover overflow-hidden`}
-    >
+    <div className="w-full h-screen mx-auto bg-[url('/public/main-bg.png')] bg-no-repeat bg-center bg-cover overflow-hidden">
+      {showSplash && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
+          <img src={currentImage} alt="Splash" className="w-full h-full object-cover" />
+        </div>
+      )}
       <Container>
         <Outlet />
       </Container>
