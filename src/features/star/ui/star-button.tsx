88dac8a70a87a3292fc4ce18ad888e8c61290753
Andrey Dremin <sankoa230@gmail.com>
2024-12-08 00:03:52 +0300

    fix: button pressing

--- src/features/star/ui/star-button.tsx
+++ src/features/star/ui/star-button.tsx
@@ -30,13 +30,26 @@ export const StarButton: FC<StarButtonProps> = ({
     setTimeout(() => setIsSpinningPossible(true), 150);
   };
 
-  const onMouseDown = () => {
+  const onMouseDown = (e) => {
+    console.log(e);
+
     if (isSpinningPossible) {
       handleMouseDown();
     }
   };
 
-
+  const onTouchMove = (e: any) => {
+    const touch = e.changedTouches[0];
+    const button = e.target.getBoundingClientRect();
+    if (
+      touch.clientX < button.left ||
+      touch.clientX > button.right ||
+      touch.clientY < button.top ||
+      touch.clientY > button.bottom
+    ) {
+      onMouseUp();
+    }
+  };
 
   return (
     <>
@@ -46,11 +59,10 @@ export const StarButton: FC<StarButtonProps> = ({
           onTouchStart={onMouseDown}
           onTouchEnd={onMouseUp}
           onMouseUp={onMouseUp}
+          onTouchMove={onTouchMove}
           onContextMenu={(e) => e.preventDefault()}
           className="bg-transparent absolute z-10 h-[45px] w-[45px] top-[calc(50%-20px)] left-[calc(50%-22.5px)]"
-        >
-          
-        </button>
+        ></button>
         <img
           ref={imgRef}
           className={`w-[292px] spin ${spinning ? "active" : ""} ${
