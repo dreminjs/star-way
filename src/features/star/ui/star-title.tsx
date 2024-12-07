585d316e32116b25b8fa3063d1acf4b53343f534
Andrey Dremin <sankoa230@gmail.com>
2024-12-07 18:45:08 +0300

    one more fix

--- src/features/star/ui/star-title.tsx
+++ src/features/star/ui/star-title.tsx
@@ -92,7 +92,7 @@ export const StarTitle: FC<IProps> = ({
   return (
     <h3
       onCopy={(e) => e.preventDefault()}
-      className="text-[#ebd0d0] text-center text-3xl w-[350px] h-[90px] mb-2 mx-auto italic "
+      className="text-[#ebd0d0] text-center text-3xl max-w-[450px] h-[90px] mb-5 mx-auto italic "
     >
       {displayedText}
     </h3>
