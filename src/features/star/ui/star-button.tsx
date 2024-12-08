8ea9090897de5c9e2b0221ece642db74bef3216e
Andrey Dremin <sankoa230@gmail.com>
2024-12-08 21:42:16 +0300

    one more fix

--- src/features/star/ui/star-button.tsx
+++ src/features/star/ui/star-button.tsx
@@ -90,7 +90,7 @@ export const StarButton: FC<StarButtonProps> = ({
       ></button>
       <img
         ref={imgRef}
-        className={`w-[70vw] spin ${spinning ? "active" : ""} ${
+        className={`w-[70vw] min-w-[420px]:w-[50vw] spin ${spinning ? "active" : ""} ${
           reset ? "reset" : ""
         }`}
         src={Star}
