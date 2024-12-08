3ea4c7a0c94fd682c61006c8d37a2eea249218a5
Andrey Dremin <sankoa230@gmail.com>
2024-12-08 21:03:41 +0300

    one more fix

--- src/app/ui/container.tsx
+++ src/app/ui/container.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export const Container: FC<IProps> = ({ children }) => {
   const queryClient = new QueryClient();
   return (
-    <div className="max-w-[420px] mx-auto h-svh" >
+    <div className="p-2 w-full mx-auto h-svh" >
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </div>
   );
