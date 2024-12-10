5a40c09d83cd0caaaefb96c8a0315ac41ed6a04e
Andrey Dremin <sankoa230@gmail.com>
2024-12-10 21:21:27 +0300

    feat: coins

--- src/features/coins/ui/coins-count.tsx
+++ src/features/coins/ui/coins-count.tsx
@@ -1,10 +1,52 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 
 interface IProps {
-    coins: number;
-  }
-  
-  export const CoinsCount: FC<IProps> = ({ coins }) => {
-    const formattedCoins = coins.toString().padStart(5, '0');
-    return <p className="text-[35px] gradient-numbers font-bold text-center gradient-coins">{formattedCoins}</p>;
-  };
+  coins: number;
+}
+
+export const CoinsCount: FC<IProps> = ({ coins }) => {
+  const [previousCoins, setPreviousCoins] = useState(coins);
+  const [addedCoins, setAddedCoins] = useState(0);
+  const [showAddedCoins, setShowAddedCoins] = useState(false);
+
+  useEffect(() => {
+    if (coins !== previousCoins) {
+      const difference = coins - previousCoins;
+      setAddedCoins(difference);
+      setShowAddedCoins(true);
+
+      const timer = setTimeout(() => {
+        setShowAddedCoins(false);
+      }, 3000); // Показывать добавленные монеты 2 секунды
+
+      // Обновить предыдущую сумму после задержки
+      const updateTimer = setTimeout(() => {
+        setPreviousCoins(coins);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timer);
+        clearTimeout(updateTimer);
+      };
+    }
+  }, [coins, previousCoins]);
+
+  const formattedCoins = coins.toString().padStart(5, "0");
+  const formattedAddedCoins = addedCoins > 0 ? `+${addedCoins}` : "";
+
+  return (
+    <div className="relative text-center">
+      {" "}
+      <p className="text-[35px] gradient-numbers font-bold gradient-coins">
+        {" "}
+        {formattedCoins}{" "}
+      </p>{" "}
+      {showAddedCoins && (
+        <p className="absolute top-3 right-[80px] text-[20px] text-yellow-500 font-bold animate-fade-out">
+          {" "}
+          {formattedAddedCoins}{" "}
+        </p>
+      )}{" "}
+    </div>
+  );
+};
