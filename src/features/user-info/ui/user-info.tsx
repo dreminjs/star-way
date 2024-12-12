d75b189037f59048a3c5242c210d58a97e3d647d
Andrey Dremin <sankoa230@gmail.com>
2024-12-13 00:28:42 +0300

    one more change

--- src/features/user-info/ui/user-info.tsx
+++ src/features/user-info/ui/user-info.tsx
@@ -6,16 +6,18 @@ interface IProps {
 }
 
 export const UserInfo: FC<IProps> = ({ tasksCount }) => {
-
-  const { userData } = useGetUserData()
+  const { userData } = useGetUserData();
 
   return (
-    <div className="flex justify-between w-[70%] mx-auto">
-      <div className="flex items-center gap-2 border-r border-[#086dd3] pr-5">
+    <div className="flex justify-between w-full mx-auto">
+      <div className="flex items-center gap-2 ">
         <p className="gradient-text text-[30px] font-bold">тапов</p>
-        <p className="gradient-numbers text-[30px] font-bold">{userData?.taps}</p>
+        <p className="gradient-numbers text-[30px] font-bold">
+          {userData?.taps}
+        </p>
       </div>
-      <div className="flex items-center gap-2 pl-5">
+      <div className="h-[40px] w-[1px] р bg-[#086dd3]"></div>
+      <div className="flex items-center gap-2">
         <p className="gradient-text text-[30px] font-bold">заданий</p>
         <p className="gradient-numbers text-[30px] font-bold">{tasksCount}</p>
       </div>
