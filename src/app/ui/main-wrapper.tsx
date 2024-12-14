a316a7f56941eccbb84e01ea1566f218ac2797a3
Andrey Dremin <sankoa230@gmail.com>
2024-12-14 20:48:02 +0300

    one more fix

--- src/app/ui/main-wrapper.tsx
+++ src/app/ui/main-wrapper.tsx
@@ -1,33 +1,30 @@
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Container } from "./container";
-import FirstLoadingImg from "../../assets/loading-page-1.png";
-import SecondLoadingImg from "../../assets/loading-page-2.png";
+import Preview from "../../assets/loading-page-2.png";
 
 export const MainWrapper = () => {
-  const [showSplash, setShowSplash] = useState(true);
-  const [currentImage, setCurrentImage] = useState(FirstLoadingImg);
+  const [isPreviwVisible, setIsPreviwVisible] = useState(true);
 
   useEffect(() => {
-    const imageTimer = setTimeout(() => {
-      setCurrentImage(SecondLoadingImg);
-    }, 1000); // Переключить изображение через 1.5 секунды
-
     const splashTimer = setTimeout(() => {
-      setShowSplash(false);
-    }, 1000); // Скрыть картинку через 3 секунды
+      setIsPreviwVisible(false);
+    }, 7000); // Скрыть картинку через 3 секунды
 
     return () => {
-      clearTimeout(imageTimer);
       clearTimeout(splashTimer);
     };
   }, []);
 
   return (
     <div className="w-full h-screen mx-auto bg-[url('/public/main-bg.png')] bg-no-repeat bg-center bg-cover overflow-hidden">
-      {showSplash && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
-          <img src={currentImage} alt="Splash" className="w-full h-full object-cover" />
+      {isPreviwVisible && (
+        <div className="fixed h-screen inset-0 z-50 flex items-center justify-center bg-white">
+          <img
+            src={Preview}
+            alt="Splash"
+            className="w-full h-full object-cover"
+          />
         </div>
       )}
       <Container>
--- src/features/awards/ui/every-day-awards.tsx
+++ src/features/awards/ui/every-day-awards.tsx
@@ -15,7 +15,7 @@ export const EveryDayAwards = () => {
 
   return (
     <div className="w-full ">
-      <div className="bg-[#1e2541] mb-2 border-[#90b0c4] border-b-[1px]">
+      <div className="bg-[#1e2541] mb-5 border-[#90b0c4] border-b-[1px]">
         {" "}
         {days?.counter && (
           <ul className="flex justify-center items-center mx-auto">
