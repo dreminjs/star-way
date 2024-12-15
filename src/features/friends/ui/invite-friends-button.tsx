b500d384bb16d910ee27c3ececd4d5b5a3f031b1
Andrey Dremin <sankoa230@gmail.com>
2024-12-15 16:25:31 +0300

    ref link

--- src/features/friends/ui/invite-friends-button.tsx
+++ src/features/friends/ui/invite-friends-button.tsx
@@ -1,8 +1,36 @@
+import React, { useState } from "react";
+
 export const InviteFriendsButton = () => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const tg = window.Telegram.WebApp.initDataUnsafe;
+
+  const handleClick = () => {
+    const link = `https://t.me/StarWayGameBot?start=sendRate-${tg.user?.id}`;
+
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        setIsCopied(true);
+      })
+      .catch((err) => {
+        console.error("Ошибка копирования: ", err);
+      });
+  };
+
   return (
-    <button className="text-[32px] text-white font-bold mx-auto block bg-[#222a44] border-[#90b0c4] border-y-[2px] w-full py-2 rounded-lg mb-3">
-      Пригласить Друга
-      <span className="gradient-text"> +</span>
+    <button
+      onClick={handleClick}
+      className="text-[32px] text-white font-bold mx-auto block bg-[#222a44] border-[#90b0c4] border-y-[2px] w-full py-2 rounded-lg mb-3"
+    >
+      {isCopied ? (
+        <>
+          <span>Пригласить друзей</span>
+          <span className="gradient-text"> +</span>
+        </>
+      ) : (
+        <span>Скопированно </span>
+      )}
     </button>
   );
 };
