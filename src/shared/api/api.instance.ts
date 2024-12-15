f2dd85083003d2b7534d12610e7c4c6675bb5835
Andrey Dremin <sankoa230@gmail.com>
2024-12-15 16:11:45 +0300

    one more fix

--- src/shared/api/api.instance.ts
+++ src/shared/api/api.instance.ts
@@ -1,10 +1,12 @@
 import { BASE_URL } from "../model/constants";
 import axios from "axios";
 
+const tg = window.Telegram.WebApp.initDataUnsafe;
+
 export const instance = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "user-id": 1413661451,
-    username: "@dreami_e",
+    "user-id": tg.user?.id,
+    "username": tg.user?.username,
   },
 });
