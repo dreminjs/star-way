785abd78fe96fe74e29ee89b4100faa3d63a6f55
Andrey Dremin <94892594+dreminjs@users.noreply.github.com>
2024-12-18 01:19:09 +0300

    Update tasks-item.tsx

--- src/entities/tasks/ui/tasks-item.tsx
+++ src/entities/tasks/ui/tasks-item.tsx
@@ -12,7 +12,8 @@ interface IProps {
 
 export const TasksItem: FC<IProps> = ({ content, link, onCheckSub, id }) => {
   return (
-    <li className="w-full flex justify-between items-center h-[50px] bg-[#222a44] p-[25px] rounded-lg border-[#90b0c4] border-y-[3px] mb-2 ">
+    <li className="w-full flex justify-center items-center h-[50px] bg-[#222a44] p-[25px] rounded-lg border-[#90b0c4] border-y-[3px] mb-2 ">
+     <div className="flex gap-3 items-center"
       <Link className="flex basis-[90%] items-center gap-2" to={link}>
         <img src={Tg} alt="" className="h-[50px]" />
         <p className="text-white text-wrap break-words">{content}</p>
@@ -20,6 +21,7 @@ export const TasksItem: FC<IProps> = ({ content, link, onCheckSub, id }) => {
       <button className="basis-[25%]" onClick={() => onCheckSub(id)}>
         <img className="h-[45px] " src={Prise} alt="" />
       </button>
+      </div>
     </li>
   );
 };
